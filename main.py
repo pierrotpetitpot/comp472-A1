74478d9825eda4b15152549e509ed480c90b0df4
pierrotpetitpot <pierretran33@gmail.com>
2021-02-17 19:30:38 -0500

    adding comments

--- main.py
+++ main.py
@@ -11,32 +11,41 @@ import collections
 
 
 split = 0
+# we instantiate the matrix with these numbers for performance
 wordcount = 54090
 instancescount = 11914
 
 labels = []
+# features is an empty matrix where each row is the number of occurrence of an unique word
+# in the review
 features = [[0]*wordcount for _ in range(instancescount)]
 word_hash = collections.defaultdict(int)
 
+# i is the count of unique words
 i=0
-with open("all_sentiment_shuffled.txt", encoding='utf8') as f:
 
+with open("all_sentiment_shuffled.txt", encoding='utf8') as f:
+# we go line by line
     for j, line in enumerate(f):
+        # we split the line by words
         line = line.split()
         if line[1]=='pos':
             labels.append(1)
         else:
             labels.append(0)
-        
+
+        #line is the actual review
         line = line [3:]
         for w in line:
-            w= w.translate (str.maketrans('', '', string.punctuation)) #TODO maybe discard numbers
+            w= w.translate (str.maketrans('', '', string.punctuation))
         if w:
+            #if the word is unique, we add it to the hash
             if w not in word_hash:
                 features[j][i] +=1
                 word_hash[w] = i
                 i += 1
             else:
+                # else we just add a count
                 features[j][word_hash[w]] +=1
 
 f.close()
